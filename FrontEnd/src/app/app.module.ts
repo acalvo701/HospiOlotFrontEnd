4f0a312fd49772b91f400b7ebe9d667435e83f55
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2023-01-16 19:34:40 +0100

    Base Html i Csss

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiaComponent } from './projecte/components/dia/dia.component';
+import { MesComponent } from './projecte/components/mes/mes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiaComponent
+    DiaComponent,
+    MesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
