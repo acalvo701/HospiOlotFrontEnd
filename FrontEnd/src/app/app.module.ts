371136f2990d19e5d7fe0b38b8725c8e77b8d7ce
JordiPadrosa <jpadrosa8@boscdelacoma.cat>
2023-01-16 20:00:29 +0100

    calendari component

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -6,17 +6,26 @@ import { AppComponent } from './app.component';
 import { DiaComponent } from './projecte/components/dia/dia.component';
 import { MesComponent } from './projecte/components/mes/mes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendariComponent } from './projecte/components/calendari/calendari.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DiaComponent,
-    MesComponent
+    MesComponent,
+    CalendariComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatNativeDateModule,
+    MatDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
