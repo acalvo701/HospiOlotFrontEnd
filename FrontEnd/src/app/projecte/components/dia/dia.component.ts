3a84abd1d2e46ea54ae6dfe9604abae9c00c2163
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2023-01-18 20:47:55 +0100

    Calendari

--- FrontEnd/src/app/projecte/components/dia/dia.component.ts
+++ FrontEnd/src/app/projecte/components/dia/dia.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dia',
   templateUrl: './dia.component.html',
   styleUrls: ['./dia.component.css']
 })
-export class DiaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class DiaComponent {
 
 }
--- FrontEnd/src/app/projecte/components/historial/historial.component.ts
+++ FrontEnd/src/app/projecte/components/historial/historial.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-historial',
   templateUrl: './historial.component.html',
   styleUrls: ['./historial.component.css']
 })
-export class HistorialComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class HistorialComponent {
 
 }
