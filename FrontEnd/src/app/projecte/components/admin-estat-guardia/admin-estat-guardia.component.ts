7a445494445c3c4a8178c14cf7499030cbe78f89
ivandpf <idelpozo8@boscdelacoma.cat>
2023-01-24 19:33:46 +0100

    Canvis subscribe

--- FrontEnd/src/app/projecte/components/admin-estat-guardia/admin-estat-guardia.component.ts
+++ FrontEnd/src/app/projecte/components/admin-estat-guardia/admin-estat-guardia.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Guardia } from '../../model/entitats/implementacions/Guardia';
 import { AdminApiService } from '../../model/services/admin/admin-api';
+import { catchError, Subscription, take, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-admin-estat-guardia',
   templateUrl: './admin-estat-guardia.component.html',
   styleUrls: ['./admin-estat-guardia.component.scss']
 })
-export class AdminEstatGuardiaComponent implements OnInit {
+export class AdminEstatGuardiaComponent implements OnInit, OnDestroy {
   estatGuardiaForm: FormGroup;
 
   guardies: Array<Guardia> = [];
 
   dataGuardia: Date;
   ocult: boolean = true;
+
+  subscription: Subscription;
+  error: string;
+  valid: string;
 
   constructor(private httpClient: AdminApiService, private fb: FormBuilder) { }
 
@@ -24,6 +29,10 @@ export class AdminEstatGuardiaComponent implements OnInit {
       idGuardia: ['', Validators.required],
       estat: ['', Validators.required]
     })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   getDataEntrada() {
@@ -42,14 +51,26 @@ export class AdminEstatGuardiaComponent implements OnInit {
   }
 
   estatGuardia() {
-
     let estatGuardia = new Guardia(this.estatGuardiaForm.get("idGuardia")?.value, this.estatGuardiaForm.get("dataGuardia")?.value, '', this.estatGuardiaForm.get("estat")?.value, '', '');
 
-    this.httpClient.updateEstatGuardiaAdmin(estatGuardia).subscribe(
-      response => {
-        console.log(response);
-      }
-    );
+    this.subscription = this.httpClient.updateEstatGuardiaAdmin(estatGuardia).
+      pipe(take(1), catchError((err: any) => {
+        return throwError(() => new Error("Error d'API"))
+      }))
+      .subscribe(
+        {
+          next: (response) => {
+            this.valid = response.message;
+          },
+          //per veure l'error que retorna de l'api
+          error: (err: any) => {
+            this.error = err.error;
+          },
+          complete: () => {
+
+          },
+        });
+
     this.ocult = true;
     this.estatGuardiaForm.reset();
   }
