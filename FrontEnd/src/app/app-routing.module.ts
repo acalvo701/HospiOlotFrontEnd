112fdbe068e30f7a3560c9870c8705d3914669b5
ivandpf <idelpozo8@boscdelacoma.cat>
2023-01-25 17:57:29 +0100

    t

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MesComponent} from './projecte/components/mes/mes.component';
 import { HistorialComponent} from './projecte/components/historial/historial.component';
-import { AdminAssignarGuardiaComponent } from './projecte/components/admin-assignar-guardia/admin-assignar-guardia.component';
 import { LoginComponent } from './projecte/components/login/login.component';
+import { AdminMainScreenComponent } from './projecte/components/admin-main-screen/admin-main-screen.component';
 
 
   const routes: Routes = [
     { path: 'mes', component: MesComponent },
     { path: 'historial', component: HistorialComponent },
-    { path: 'admin', component: AdminAssignarGuardiaComponent },
+    { path: 'admin', component: AdminMainScreenComponent },
     { path: 'login', component: LoginComponent },
   ];
 
