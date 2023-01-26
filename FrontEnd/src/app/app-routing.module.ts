e467ef753343ebf8df20c2ea740e9882894a462e
acalvo701 <93265280+acalvo701@users.noreply.github.com>
2023-01-26 23:39:46 +0100

    merge conflict resolved

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { MesComponent} from './projecte/components/mes/mes.component';
 import { HistorialComponent} from './projecte/components/historial/historial.component';
 import { LoginComponent } from './projecte/components/login/login.component';
 import { AdminMainScreenComponent } from './projecte/components/admin-main-screen/admin-main-screen.component';
+import { AuthGuard } from './projecte/guards/auth.guard';
 
 
   const routes: Routes = [
-    { path: 'mes', component: MesComponent },
+    { path: 'mes', component: MesComponent, canActivate: [AuthGuard] },
     { path: 'historial', component: HistorialComponent },
     { path: 'admin', component: AdminMainScreenComponent },
     { path: 'login', component: LoginComponent },
