fca37948d0d29bc6d383e753da915cd4fb07768c
acalvo701 <93265280+acalvo701@users.noreply.github.com>
2023-01-30 15:11:03 +0100

    ip change

--- FrontEnd/src/app/projecte/model/services/guardia/guardia-api.service.ts
+++ FrontEnd/src/app/projecte/model/services/guardia/guardia-api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class GuardiaApiService {
   IP: string;
 
   constructor(private http: HttpClient) {
-    this.IP = "localhost";
+    this.IP = environment.ip;
   }
 
   getHistoryTreballador(idTreballador: number): Observable<any> {
